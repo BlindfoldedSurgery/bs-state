c2ded571699b54471bce04bd5d684a8716723ada
Björn Petersen <git@bjoernpetersen.net>
2023-10-21 20:03:03 +0200

    Run k8s tests (#5)
    * Load Kubeconfig as JSON
    
    * Revert test job
    
    * Handle 404 in ConfigMapStorage.load()
    
    * Store data under a single key
    
    * Use slugified test name for ConfigMap name
    
    * Create ConfigMap before replacing it
    
    * Use model_validate_json
    
    * Cleanup

--- src/bs_state/implementation/config_map_storage.py
+++ src/bs_state/implementation/config_map_storage.py
@@ -2,6 +2,7 @@ from asyncio import Lock
 from importlib.util import find_spec
 from typing import Any, Generic, Self, Type, TypeVar
 
+from kubernetes_asyncio.client import ApiException, V1ConfigMap, V1ObjectMeta
 from pydantic import BaseModel
 
 from bs_state import MissingStateException, StateStorage
@@ -30,6 +31,8 @@ async def load(
 
 
 class _ConfigMapStateStorage(StateStorage[T], Generic[T]):
+    DATA_KEY = "state"
+
     def __init__(
         self,
         state_type: Type[T],
@@ -65,11 +68,34 @@ class _ConfigMapStateStorage(StateStorage[T], Generic[T]):
             # See if there are values
             await storage.load()
         except MissingStateException:
+            await storage._create_config_map()
             await storage.store(initial_state)
         return storage
 
+    @property
+    def _metadata(self) -> V1ObjectMeta:
+        return V1ObjectMeta(
+            namespace=self._namespace,
+            name=self._config_map_name,
+        )
+
+    async def _create_config_map(self) -> None:
+        async with client.ApiClient() as api:
+            v1 = client.CoreV1Api(api)
+            async with self._lock:
+                await v1.create_namespaced_config_map(
+                    namespace=self._namespace,
+                    body=V1ConfigMap(
+                        metadata=self._metadata,
+                        data={},
+                    ),
+                )
+
     async def store(self, state: T) -> None:
-        config_map = client.V1ConfigMap(data=state.model_dump_json())
+        config_map = client.V1ConfigMap(
+            metadata=self._metadata,
+            data={self.DATA_KEY: state.model_dump_json()},
+        )
         async with client.ApiClient() as api:
             v1 = client.CoreV1Api(api)
             async with self._lock:
@@ -83,9 +109,19 @@ class _ConfigMapStateStorage(StateStorage[T], Generic[T]):
         async with client.ApiClient() as api:
             v1 = client.CoreV1Api(api)
             async with self._lock:
-                config_map = await v1.read_namespaced_config_map(
-                    name=self._config_map_name,
-                    namespace=self._namespace,
-                )
+                try:
+                    config_map = await v1.read_namespaced_config_map(
+                        name=self._config_map_name,
+                        namespace=self._namespace,
+                    )
+                except ApiException as e:
+                    if e.status == 404:
+                        raise MissingStateException()
+                    raise e
 
-        return self._type.model_validate(config_map.data)
+        data = config_map.data.get(self.DATA_KEY)
+
+        if not data:
+            raise MissingStateException()
+
+        return self._type.model_validate_json(data)
--- src/tests/implementation/test_config_map_storage.py
+++ src/tests/implementation/test_config_map_storage.py
@@ -1,28 +1,33 @@
+import json
 import subprocess
 from typing import Any, cast
 
 import pytest
-import yaml
 
 from bs_state import StateStorage
 from bs_state.implementation import config_map_storage
 from tests.implementation.conftest import ImplementationTest, T
 
 
-@pytest.mark.skip
 @pytest.mark.kubernetes
 class TestConfigMapState(ImplementationTest):
     @pytest.fixture
     def kubeconfig(self) -> dict[str, Any]:
         process = subprocess.run(
-            args=["kubectl", "config", "view"],
+            args=["kubectl", "config", "view", "-o=json"],
             capture_output=True,
+            timeout=3,
         )
 
         if (exit_code := process.returncode) != 0:
             raise OSError(f"kubectl config view return code {exit_code}")
 
-        return cast(dict[str, Any], yaml.safe_load(process.stdout))
+        result = cast(dict[str, Any], json.loads(process.stdout))
+        return result
+
+    @staticmethod
+    def slugify(method_name: str) -> str:
+        return method_name.replace("_", "-").replace(".", "-")
 
     @pytest.fixture
     def storage_factory(self, kubeconfig, request):
@@ -30,7 +35,7 @@ class TestConfigMapState(ImplementationTest):
             return await config_map_storage.load(
                 initial_state=state,
                 namespace="default",
-                config_map_name=request.node.name,
+                config_map_name=self.slugify(request.node.name),
                 kubeconfig=kubeconfig,
             )
 
