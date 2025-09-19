752b6bdf0198a234bd12094533e0355be8e14019
Björn Petersen <git@bjoernpetersen.net>
2025-09-19 09:40:18 +0200

    feat: add opentelemetry integration

--- src/bs_state/implementation/file_storage.py
+++ src/bs_state/implementation/file_storage.py
@@ -2,6 +2,7 @@ from asyncio.locks import Lock
 from pathlib import Path
 from typing import Self
 
+from opentelemetry import trace
 from pydantic import BaseModel
 
 from bs_state import AccessException, MissingStateException, StateStorage
@@ -11,7 +12,10 @@ try:
 except ImportError:
     raise RuntimeError("Requires extra 'file', i.e. bs-state[file]")
 
+_tracer = trace.get_tracer(__name__)
 
+
+@_tracer.start_as_current_span("load")
 async def load[T: BaseModel](*, initial_state: T, file: Path) -> StateStorage[T]:
     return await _FileStateStorage.initialize(initial_state, file)
 
@@ -32,6 +36,7 @@ class _FileStateStorage[T: BaseModel](StateStorage[T]):
             await storage.store(initial_state)
         return storage
 
+    @_tracer.start_as_current_span("store")
     async def store(self, state: T) -> None:
         json = state.model_dump_json()
         async with self._lock:
@@ -41,6 +46,7 @@ class _FileStateStorage[T: BaseModel](StateStorage[T]):
             except OSError as e:
                 raise AccessException from e
 
+    @_tracer.start_as_current_span("load")
     async def load(self) -> T:
         model = self._type
         async with self._lock:
--- src/bs_state/implementation/valkey_storage.py
+++ src/bs_state/implementation/valkey_storage.py
@@ -3,6 +3,7 @@ from asyncio.locks import Lock
 from importlib.util import find_spec
 from typing import Self
 
+from opentelemetry import trace
 from pydantic import BaseModel
 
 from bs_state import AccessException, MissingStateException, StateStorage
@@ -11,9 +12,13 @@ if find_spec("valkey") is None:
     raise RuntimeError("Requires extra 'valkey', i.e. bs-state[valkey]")
 
 import valkey.asyncio as valkey
+from opentelemetry.instrumentation.redis import RedisInstrumentor
 from valkey.exceptions import AuthenticationError, ValkeyError
 
+_tracer = trace.get_tracer(__name__)
 
+
+@_tracer.start_as_current_span("load")
 async def load[T: BaseModel](
     *,
     initial_state: T,
@@ -30,6 +35,9 @@ async def load[T: BaseModel](
         username=username,
         protocol=3,
     )
+
+    RedisInstrumentor().instrument_client(client)  # type: ignore
+
     try:
         await client.ping()
     except AuthenticationError as e:
@@ -50,6 +58,7 @@ class _ValkeyStateStorage[T: BaseModel](StateStorage[T]):
         self._lock = Lock()
 
     @classmethod
+    @_tracer.start_as_current_span("initialize")
     async def initialize(
         cls,
         initial_state: T,
@@ -65,6 +74,7 @@ class _ValkeyStateStorage[T: BaseModel](StateStorage[T]):
             await storage.store(initial_state)
         return storage
 
+    @_tracer.start_as_current_span("store")
     async def store(self, state: T) -> None:
         json = state.model_dump_json()
         async with self._lock:
@@ -73,6 +83,7 @@ class _ValkeyStateStorage[T: BaseModel](StateStorage[T]):
             except ValkeyError as e:
                 raise AccessException("Could not update value in Valkey") from e
 
+    @_tracer.start_as_current_span("load")
     async def load(self) -> T:
         model = self._type
         async with self._lock:
@@ -86,6 +97,7 @@ class _ValkeyStateStorage[T: BaseModel](StateStorage[T]):
 
             return model.model_validate_json(result)
 
+    @_tracer.start_as_current_span("close")
     async def close(self) -> None:
         async with self._lock:
             await self._client.aclose()
